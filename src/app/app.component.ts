d7134441809c1d73622b25bf63e121f68f1a9f9d
Benoit Mansoux <benoit.mansoux@gmail.com>
2024-06-23 19:44:48 +0200

    [app][wip] use album-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { details } from './data/details';
+import { AlbumListComponent } from './album-list/album-list.component';
+
+type AlbumCover = {
+  "src": string;
+  "alt": string;
+};
+
+type AlbumDetail =
+  { "id": number;  
+    "leader": string;
+    "title": string;
+    "year": number;
+    "description": string;
+    "cover": AlbumCover;
+    "type": string;
+    "label": string;
+    "distributor": string;
+    "essential": boolean;
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive ],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, AlbumListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ideal-jazz';
+  title:string =  'ideal-jazz';
+  albumList: {id: number; leader: string; title: string; year: number;}[] = details.map((item: AlbumDetail) => { return {id: item.id, leader: item.leader, title: item.title, year: item.year}});
+  
 }
