af410110a570695399c04bb062a74f2e554c953f
Benoit Mansoux <benoit.mansoux@gmail.com>
2024-06-23 19:44:17 +0200

    [data][wip] add a small set of static data

---
+++ src/app/data/details.ts
@@ -0,0 +1,30 @@
+export const details = [
+  { "id": 0, 
+    "leader": "Ahmad Jamal",
+    "title": "At the Pershing",
+    "year": 1958,
+    "description": "On l'a souvent remarqué dans ses enregistrements : Ahmad Jamal est autant un pianiste qu'un leader en trio, genre où il excelle. Sa particularité était d'ếtre économe et \"passeur\", aménageant de larges espaces de silence, ou les interventions des autres musiciens. Depuis sur ces derniers enregistrements, on l'a connu plus prolixe et moins effacé... At the Pershing, enregsitré en public, est révélateur du Jamal première période. Il entretient un dialogue intelligent avec ses complices (le batteur Vernell Fournier et le contrebassiste Israel Crosby). Ce disque remporta à sa sortie, un très large succès public. On comprend pourquoi.",
+    "cover": {
+      "src": "",
+      "alt": "Live at the Pershing cover"
+    },
+    "type": "hardbop",
+    "label": "Chess MCD 09108",
+    "distributor": "Universal",
+    "essential": false
+  },
+  { "id": 1, 
+    "leader": "Louis Armstrong",
+    "title": "Louis and the Good Book, volume 1",
+    "year": 1958,
+    "description": "Louis, le trompettiste swing  d'un côté et un répertoire de chants traditionnels et de \"spirituals\" de l'autre. ",
+    "cover": {
+      "src": "",
+      "alt": "Louis and the Good Book, volume 1"
+    },
+    "type": "swing",
+    "label": "MCA MCD 01300",
+    "distributor": "Universal",
+    "essential": true
+  }
+]
